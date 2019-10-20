7884acedfebf9e16c9662a3a26f083adcf22bfa8
cedrickevers <keverscedric@gmail.com>
2019-10-20 20:52:05 +0200

    et a landing routes

--- server.js
+++ server.js
@@ -11,13 +11,12 @@ const app = express();
 app.engine('.hbs', expressHbs({ defaultLayout: 'layout', extname: '.hbs' }));
 app.set('view engine', 'hbs');app.set('view engine', 'hbs')
 app.use(morgan('dev'));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public')); 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.get('/', (req, res, next) => {
-    res.render('main/home');
-});
-
+ 
+const mainRoutes = require("./routes/main") ;
+app.use(mainRoutes);
 app.listen(7777, (err) =>{
     if(err){
         console.log(err)

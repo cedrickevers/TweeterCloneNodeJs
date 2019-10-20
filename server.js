0dadb671ad1e0e75a552660f5aaa45bde9ca9f43
cedrickevers <keverscedric@gmail.com>
2019-10-20 19:04:05 +0200

    add Hbs and boostrap nav

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
-const request = require("request");
-const async = require("async");
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const request = require('request');
+const async = require('async');
+const expressHbs = require ('express-handlebars');
+
+
  
 const app = express();
-app.use(bodyParser.json);
-app.use(bodyParser.urlencoded({extended : false}));
-app.use(morgan("dev"));
-
-app.get("/", (req, res, next) => {
-    res.json("test")
+app.engine('.hbs', expressHbs({ defaultLayout: 'layout', extname: '.hbs' }));
+app.set('view engine', 'hbs');app.set('view engine', 'hbs')
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/', (req, res, next) => {
+    res.render('main/home');
 });
 
 app.listen(7777, (err) =>{
     if(err){
         console.log(err)
     }
-    else console.log("All seven wins !")
+    else console.log('All seven wins !')
 })
 
 
